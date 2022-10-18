8239498f6eaa1c63cf3859933da5af341b44f1e0
Paulius Buitvydas <paulius.buitvydas@juvare.com>
2022-10-18 16:44:49 +0300

    update to load agent outside gradle

--- start-example.sh
+++ start-example.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+export TREND_AP_KEY=
+export TREND_AP_SECRET=
 
 ./gradlew build
 
 docker build -t jobrunr-trendmicro webapp/docker
 
-docker run -p8080:8080 jobrunr-trendmicro:latest
+docker run --rm -p8080:8080 -e TREND_AP_KEY=$TREND_AP_KEY -e TREND_AP_SECRET=$TREND_AP_SECRET jobrunr-trendmicro:latest
